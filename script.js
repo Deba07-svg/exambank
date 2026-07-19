3416efc717504e23d3358d14456efce621ee42d0
Deba07-svg <tamulidebajit9@gmail.com>
2026-07-19 10:19:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 function searchMenu() {
-  let input = document.getElementById("searchBox").value.toLowerCase();
-  let items = document.getElementsByClassName("menu-item");
 
-  for (let i = 0; i < items.length; i++) {
-    let text = items[i].innerText.toLowerCase();
+let input = document.getElementById("searchBox").value.toLowerCase();
 
-    if (text.includes(input)) {
-      items[i].style.display = "block";
-    } else {
-      items[i].style.display = "none";
-    }
-  }
+let items = document.getElementsByClassName("menu-item");
+
+
+for(let i=0; i<items.length; i++){
+
+let text = items[i].innerText.toLowerCase();
+
+
+if(text.includes(input)){
+
+items[i].style.display="block";
+
+}
+
+else{
+
+items[i].style.display="none";
+
+}
+
+}
+
 }
